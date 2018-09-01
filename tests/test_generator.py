c367f05de4debc48b15c35f79f3d40bf674bff79
rrozansk <nrr1228@gmail.com>
2018-09-01 07:43:48 -0700

    Update tests to use assert statements.

--- tests/test_generator.py
+++ tests/test_generator.py
@@ -18,63 +18,16 @@ class TestGenerator(object):
         Ensure successful creation of a Generator object with no scanner or
         parser.
         """
-        Generator(None, None)
+        generator = Generator(None, None)
+        assert generator is not None, 'Invalid Generator produced'
 
     @staticmethod
-    def test_constructor_scanner():
+    def test_constructor_valid_scanner():
         """
         Ensure successful creation of a Generator object with only a scanner.
         """
-        Generator(RegularGrammar('test', []), None)
-
-    @staticmethod
-    def test_constructor_parser():
-        """
-        Ensure successful creation of a Generator object with only a parser.
-        """
-        Generator(None, ContextFreeGrammar('test', {'S': ''}, 'S'))
-
-    @staticmethod
-    def test_constructor():
-        """
-        Ensure successful creation of a Generator object with no scanner and
-        parser.
-        """
-        scanner = RegularGrammar('test', [])
-        parser = ContextFreeGrammar('test', {'S': ''}, 'S')
-        Generator(scanner, parser)
-
-    @staticmethod
-    def test_get_scanner():
-        """
-        Ensure get_scanner works correctly.
-        """
         generator = Generator(RegularGrammar('test', []), None)
-        generator.get_scanner()
-
-    @staticmethod
-    def test_set_scanner():
-        """
-        Ensure set_scanner works correctly when given proper input.
-        """
-        generator = Generator(RegularGrammar('test', []), None)
-        generator.set_scanner(RegularGrammar('test', []))
-
-    @staticmethod
-    def test_get_parser():
-        """
-        Ensure get_parser works correctly.
-        """
-        generator = Generator(None, ContextFreeGrammar('test', {'S': ''}, 'S'))
-        generator.get_parser()
-
-    @staticmethod
-    def test_set_parser():
-        """
-        Ensure set_parser works correctly when given proper input.
-        """
-        generator = Generator(None, ContextFreeGrammar('test', {'S': ''}, 'S'))
-        generator.set_parser(ContextFreeGrammar('test', {'S': ''}, 'S'))
+        assert generator is not None, 'Invalid Generator produced'
 
     @staticmethod
     @pytest.mark.xfail(
@@ -83,23 +36,18 @@ class TestGenerator(object):
     )
     def test_constructor_invalid_scanner():
         """
-        Ensure an error is thrown when constructing a Generator object if the
+        Ensure a TypeError is thrown when constructing a Generator object if the
         scanner is not of type RegularGrammar.
         """
         Generator('invalid_scanner', None)
 
     @staticmethod
-    @pytest.mark.xfail(
-        reason='Scanner is not of type RegularGrammar.',
-        raises=TypeError,
-    )
-    def test_set_invalid_scanner():
+    def test_constructor_valid_parser():
         """
-        Ensure an error is thrown when setting the Generator's object scanner if
-        is not of type RegularGrammar.
+        Ensure successful creation of a Generator object with only a parser.
         """
-        generator = Generator(None, None)
-        generator.set_scanner('invalid_scanner')
+        generator = Generator(None, ContextFreeGrammar('test', {'S': ''}, 'S'))
+        assert generator is not None, 'Invalid Generator produced'
 
     @staticmethod
     @pytest.mark.xfail(
@@ -108,10 +56,74 @@ class TestGenerator(object):
     )
     def test_constructor_invalid_parser():
         """
-        Ensure an error is thrown when constructing a Generator object if the
+        Ensure a TypeError is thrown when constructing a Generator object if the
         parser is not of type ContextFreeGrammar.
         """
         Generator(None, 'invalid_parser')
+
+    @staticmethod
+    def test_constructor_full():
+        """
+        Ensure successful creation of a Generator object with both scanner and
+        parser.
+        """
+        scanner = RegularGrammar('test', [])
+        parser = ContextFreeGrammar('test', {'S': ''}, 'S')
+        generator = Generator(scanner, parser)
+        assert generator is not None, 'Invalid Generator produced'
+
+    @staticmethod
+    def test_get_scanner():
+        """
+        Ensure get_scanner works as expected.
+        """
+        scanner = RegularGrammar('test', [])
+        generator = Generator(scanner, None)
+        assert generator.get_scanner() == scanner, 'Invalid scanner retrieved'
+
+    @staticmethod
+    def test_set_scanner():
+        """
+        Ensure set_scanner works as expected when given proper input.
+        """
+        scanner_1 = RegularGrammar('test', [])
+        generator = Generator(scanner_1, None)
+        scanner_2 = RegularGrammar('test', [])
+        generator.set_scanner(scanner_2)
+        assert generator.get_scanner() == scanner_2, 'Invalid scanner retrieved'
+
+    @staticmethod
+    @pytest.mark.xfail(
+        reason='Scanner is not of type RegularGrammar.',
+        raises=TypeError,
+    )
+    def test_set_invalid_scanner():
+        """
+        Ensure a TypeError is thrown when setting the Generator's object scanner
+        if is not of type RegularGrammar.
+        """
+        generator = Generator(None, None)
+        generator.set_scanner('invalid_scanner')
+
+    @staticmethod
+    def test_get_parser():
+        """
+        Ensure get_parser works as expected.
+        """
+        parser = ContextFreeGrammar('test', {'S': ''}, 'S')
+        generator = Generator(None, parser)
+        assert generator.get_parser() == parser, 'Invalid parser retrieved'
+
+    @staticmethod
+    def test_set_parser():
+        """
+        Ensure set_parser works as expected when given proper input.
+        """
+        parser_1 = ContextFreeGrammar('test', {'S': ''}, 'S')
+        generator = Generator(None, parser_1)
+        parser_2 = ContextFreeGrammar('test', {'S': ''}, 'S')
+        generator.set_parser(parser_2)
+        assert generator.get_parser() == parser_2, 'Invalid parser retrieved'
 
     @staticmethod
     @pytest.mark.xfail(
@@ -120,8 +132,8 @@ class TestGenerator(object):
     )
     def test_set_invalid_parser():
         """
-        Ensure an error is thrown when setting the Generator's object parser if
-        is not of type ContextFreeGrammar.
+        Ensure a TypeError is thrown when setting the Generator's object parser
+        if is not of type ContextFreeGrammar.
         """
         generator = Generator(None, None)
         generator.set_parser('invalid_parser')
@@ -133,7 +145,7 @@ class TestGenerator(object):
     )
     def test_output_invalid_filename():
         """
-        Ensure an error is thrown if the filename is not a string.
+        Ensure a TypeError is thrown if the filename is not a string.
         """
         generator = Generator(None, None)
         generator.output(None)
@@ -143,10 +155,10 @@ class TestGenerator(object):
         reason='Output not handled by base Generator.',
         raises=NotImplementedError,
     )
-    def test_output_invalid():
+    def test_output_not_implemented():
         """
-        Ensure an error is thrown if generation to source language is attempted
-        with the base Generator.
+        Ensure a NotImplementedError is thrown if generation to source language
+        is attempted with the base Generator.
         """
         generator = Generator(None, None)
         generator.output('test_failure.txt')
