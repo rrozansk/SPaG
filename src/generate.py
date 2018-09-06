2924d023bea91098e9d1aff3b3ca6a23c17057df
rrozansk <nrr1228@gmail.com>
2018-09-06 08:38:48 -0700

    Updates and cleanup for CLI script.

--- src/generate.py
+++ src/generate.py
@@ -1,204 +1,167 @@
+#! /usr/bin/python
+# pylint: disable=too-few-public-methods
+
 """
-A python CLI program to drive the scanner/parser generation.
-
-for help/information about [in/out]put run the following at the command line:
-
-  $ python -m scanner_parser_generator.generate --help
+A python CLI script to drive the scanner/parser generation. It deals with all
+the file I/O including the definition of the input specification. It also
+properly handles the dynamic importing required for the generators of interest.
 """
+from argparse import ArgumentParser, Action
+from os.path import isfile
+from sys import stdout
+from time import time
+from src.generators import __all__ as languages
+from src.parser.parser import ContextFreeGrammar
+from src.scanner.scanner import RegularGrammar
 
-def collect_specification(_file):
+
+class DynamicGeneratorImport(Action):
+    """
+    Dynamically import the required generators for src code output.
+    """
+
+    def __call__(self, parser, namespace, values, option_string=None):
+        generators = []
+        for language in values:
+            cls = language.capitalize()
+            module = __import__('src.generators.'+language, fromlist=[cls])
+            generator = getattr(module, cls)
+            generators.append(generator)
+        setattr(namespace, self.dest, generators)
+
+class CollectSpecification(Action):
     """
     Collect the specification for a scanner and/or parser from a file.
-
-    Input Type:
-      _file: File
-
-    Output Type: Tuple[String, List[Tuple[String, String]]] | raise ValueError
     """
-    if not isinstance(_file, file):
-        raise ValueError('Error: _file must be of type file')
 
-    name, rules = None, []
+    def __call__(self, parser, namespace, values, option_string=None):
+        name, rules = None, []
+        for line in values:
+            items = line.split(None, 1)
+            if not items:
+                continue
 
-    for line in _file:
-        items = line.split(None, 1)
-        if not items:
-            continue
+            if name is None:
+                if len(items) != 1:
+                    raise ValueError('Invalid file format - name')
+                name = items[0]
+                continue
+
+            if len(items) != 2:
+                raise ValueError('Invalid file format - rule')
+
+            rules.append((items[0], items[1].rstrip()))
 
         if name is None:
-            if len(items) != 1:
-                raise ValueError('Error: Invalid file format - name')
-            name = items[0]
-            continue
+            raise ValueError('Specification must be non empty.')
 
-        if len(items) != 2:
-            raise ValueError('Error: Invalid file format - rule')
+        setattr(namespace, self.dest, {'name': name, 'rules': rules})
 
-        rules.append((items[0], items[1].rstrip()))
+CLI = ArgumentParser(
+    prog='Scanner Parser Generator',
+    usage='$ python -m scanner_parser_generator.generate --help',
+    description='''
+    Simple CLI (Command Line Interface) program to generate lexer(s) and/or
+    parser(s) for a given input specification to a set of specified output
+    languages. For information on data input, capabilities, limitation and
+    more see the README or have a look at src/scanner/scanner.py and
+    src/parser/parser.py located here:
+    https://github.com/rrozansk/Scanner-Parser-Generator
+    ''',
+    # epilog='TODO'
+)
 
-    return name, rules
+CLI.add_argument('-V', '--version', action='version',
+                 version='Lexer/Parser Generator v1.0.0a0',
+                 help='show version information and exit')
+CLI.add_argument('-f', '--force', action='store_true',
+                 help='overwrite output file(s) if already present')
+CLI.add_argument('-g', '--generate', type=str, nargs='+', default=[],
+                 choices=languages, required=True,
+                 action=DynamicGeneratorImport,
+                 help='target language(s) for code generation')
+CLI.add_argument('-o', '--output', action='store', type=str, default='out',
+                 help='base filename to use for generated output')
+CLI.add_argument('-p', '--parser', type=open, action=CollectSpecification,
+                 help='file containing parser name and LL(1) BNF grammar')
+CLI.add_argument('-s', '--scanner', type=open, action=CollectSpecification,
+                 help='file containing scanner name and type/token pairs')
+CLI.add_argument('-t', '--time', action='store_true',
+                 help='display the wall time taken for each component')
+CLI.add_argument('-v', '--verbose', action='store_true',
+                 help='output more information when running')
 
-
-if __name__ == '__main__':
-    from argparse import ArgumentParser
-    from importlib import import_module
-    from os.path import isfile
-    from time import time
-
-    from scanner.scanner import RegularGrammar
-    from parser.parser import ContextFreeGrammar
-    from generators import __all__ as languages
-
-    CLI = ArgumentParser(
-        description='''
-        Simple CLI (Command Line Interface) program to generate lexer(s) and/or
-        parser(s) for a given input specification. For information on data input,
-        capabilities, limitation and more see the README or have a look at
-        scanner_parser_generator/{scanner,parser}.py located here:
-        https://github.com/rrozansk/Scanner-Parser-Generator
-        '''
-    )
-
-    CLI.add_argument('-V', '--version', action='version',
-                     version='Lexer/Parser Generator v0.0.17',
-                     help='show version information and exit')
-    CLI.add_argument('-f', '--force', action='store_true',
-                     help='overwrite output file(s) if already present')
-    CLI.add_argument('-g', '--generate', action='store', type=str,
-                     required=True, choices=languages,
-                     help='target language for code generation')
-    CLI.add_argument('-o', '--output', action='store', type=str,
-                     help='base filename to use for generated output')
-    CLI.add_argument('-p', '--parser', action='store', type=open,
-                     help='file containing parser name and LL(1) BNF grammar')
-    CLI.add_argument('-s', '--scanner', action='store', type=open,
-                     help='file containing scanner name and type/token pairs')
-    CLI.add_argument('-t', '--time', action='store_true',
-                     help='display the wall time taken for each component')
-    CLI.add_argument('-v', '--verbose', action='store_true',
-                     help='output more information when running')
-
+try:
     ARGS = vars(CLI.parse_args())
-
-    LANGUAGE = ARGS['generate']
-
-    GENERATOR = getattr(import_module('scanner_parser_generator.generators.' + LANGUAGE),
-                        LANGUAGE.capitalize(),
-                        None)
-    if GENERATOR is None:
-        print('Error: Expected generator not found: ', LANGUAGE)
-        exit(0)
-
     START, END = None, None
 
     SCANNER = None
-
     if ARGS['scanner'] is not None:
-        try:
-            if ARGS['verbose']:
-                print('Collecting scanner specification from file.')
-
-            NAME, TOKENS = collect_specification(ARGS['scanner'])
-
-            if ARGS['verbose']:
-                print('Collected specification: {0}'.format(NAME))
-
-            if ARGS['verbose']:
-                print('Compiling specification...')
-
-            if ARGS['time']:
-                START = time()
-
-            SCANNER = RegularGrammar(NAME, TOKENS)
-
-            if ARGS['time']:
-                END = time()
-
-            if ARGS['verbose']:
-                print('Finished building scanner')
-
-            if ARGS['time']:
-                print('Time elapsed (scanner): {0}'.format(END - START))
-        except ValueError as exception:
-            print('Scanner creation failed: {0}'.format(exception))
-            exit(0)
-        except Exception as exception:
-            print('Unknown error encountered: {0}'.format(exception))
-            exit(0)
+        if ARGS['verbose']:
+            stdout.write('Compiling scanner specification...')
+            stdout.flush()
+        START = time()
+        SCANNER = RegularGrammar(ARGS['scanner']['name'],
+                                 dict(ARGS['scanner']['rules']))
+        END = time()
+        if ARGS['verbose']:
+            stdout.write('done\n')
+            stdout.flush()
+        if ARGS['time']:
+            stdout.write('Elapsed time (scanner): {0}\n'.format(END-START))
+            stdout.flush()
 
     PARSER = None
-
     if ARGS['parser'] is not None:
-        try:
-            if ARGS['verbose']:
-                print('Collecting parser specification from file.')
-
-            NAME, PRODUCTIONS = collect_specification(ARGS['parser'])
-            STARTING = PRODUCTIONS[0][0]
-
-            if ARGS['verbose']:
-                print('Collected parser specification: {0}'.format(NAME))
-
-            if ARGS['time']:
-                START = time()
-
-            PARSER = ContextFreeGrammar(NAME, dict(PRODUCTIONS), STARTING)
-
-            if ARGS['time']:
-                END = time()
-
-            if ARGS['verbose']:
-                print('Finished building parser')
-
-            if ARGS['time']:
-                print('Time elapsed (parser): {0}'.format(END - START))
-        except ValueError as exception:
-            print('Parser creation failed: {0}'.format(exception))
-            exit(0)
-        except Exception as exception:
-            print('Unknown error encountered: {0}'.format(exception))
-            exit(0)
-
-    if PARSER is None and SCANNER is None:
-        print('Error: Must provide atleast a scanner or a parser')
-        exit(0)
-
-    try:
         if ARGS['verbose']:
-            print('Translating compiled scanner and/or parser to: {0}'.format(LANGUAGE))
-
-        GENERATOR = GENERATOR(scanner=SCANNER, parser=PARSER)
-
+            stdout.write('Compiling parser specification...')
+            stdout.flush()
+        START = time()
+        PARSER = ContextFreeGrammar(ARGS['parser']['name'],
+                                    dict(ARGS['parser']['rules']),
+                                    ARGS['parser']['rules'][0][0])
+        END = time()
+        if ARGS['verbose']:
+            stdout.write('done\n')
+            stdout.flush()
         if ARGS['time']:
-            START = time()
+            stdout.write('Elapsed time (parser): {0}\n'.format(END-START))
+            stdout.flush()
 
+    for GENERATOR in ARGS['generate']:
+        GENERATOR = GENERATOR(SCANNER, PARSER)
+        if ARGS['verbose']:
+            stdout.write('Generating {0} code...'.format(type(GENERATOR)))
+            stdout.flush()
+        START = time()
         FILES = GENERATOR.output(ARGS['output'])
-
-        if ARGS['time']:
-            END = time()
-
+        END = time()
         if ARGS['verbose']:
-            print('Finished building output files.')
-
+            stdout.write('done\n')
+            stdout.flush()
         if ARGS['time']:
-            print('Time elapsed (generator): {0}'.format(END - START))
+            stdout.write('Elapsed time (generator: {0}): {1}\n'.format(type(GENERATOR),
+                                                                       END-START))
+            stdout.flush()
 
-        if ARGS['verbose']:
-            print('Outputting files to disk...')
-
-        for _file, content in FILES:
-            if isfile(_file) and not ARGS['force']:
-                print('{0} already exists. Use --force (-f) to overwrite.'.format(_file))
+        for NAME, CONTENT in FILES.items():
+            if isfile(NAME) and not ARGS['force']:
+                if ARGS['verbose']:
+                    stdout.write('{0} already exists; not overwriting.\n'.format(NAME))
+                    stdout.flush()
                 continue
 
-            with open(_file, 'w') as fd:
-                fd.write(content)
-
-            if ARGS['verbose']:
-                print(_file)
-    except ValueError as exception:
-        print('Generator program compilation failed: {0}'.format(exception))
-        exit(0)
-    except Exception as exception:
-        print('Unknown error encountered: {0}'.format(exception))
-        exit(0)
+            with open(NAME, 'w') as FILE:
+                if ARGS['verbose']:
+                    stdout.write('Outputting {0} to disk...\n'.format(FILE.name))
+                    stdout.flush()
+                FILE.write(CONTENT)
+except TypeError as exception:
+    stdout.write('Invalid input type: {0}\n'.format(exception))
+except ValueError as exception:
+    stdout.write('Invalid input value: {0}\n'.format(exception))
+except Exception as exception:
+    stdout.write('Unknown exception encountered: {0}\n'.format(exception))
+finally:
+    stdout.flush()
+    exit(0)
